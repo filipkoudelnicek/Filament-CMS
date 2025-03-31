280ba0bc32e97c965c6741ce7c7b9d8c21938b80
filipkoudelnicek@seznam.cz <62012471+filipkoudelnicek@users.noreply.github.com>
2025-03-31 22:29:35 +0200

    Add sitemap generation functionality; integrate Spatie Laravel Sitemap and update routes for multilingual support

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\ArticleController;
 use App\Models\Page;
+use App\Models\Article;
 use App\Models\Language;
+use Spatie\Sitemap\Sitemap;
+use Spatie\Sitemap\Tags\Url;
 
 $languages = Language::where('active', true)->get();
 
@@ -21,6 +24,48 @@ $blogSlug = $blogPage ? $blogPage->slug : 'blog';
 
 Route::get('/' . $blogSlug . '/{articleSlug}', [ArticleController::class, 'showArticle'])
     ->name('article.show');
+
+Route::get('/sitemap', function() {
+    $sitemap = Sitemap::create();
+    $languages = Language::where('active', true)->get();
+    $defaultLocale = 'cs';
+    
+    $sitemap->add(Url::create(url('/')));
+    
+    foreach ($languages as $language) {
+        if ($language->locale !== $defaultLocale) {
+            $sitemap->add(Url::create(url('/' . $language->locale)));
+        }
+    }
+    
+    Page::where('active', true)->get()
+        ->each(function ($page) use ($sitemap, $defaultLocale) {
+            if ($page->type === 'homepage') return;
+            
+            $pageUrl = $page->lang_locale === $defaultLocale 
+                ? url('/' . $page->slug) 
+                : url('/' . $page->lang_locale . '/' . $page->slug);
+            
+            $sitemap->add(Url::create($pageUrl));
+        });
+    
+    Article::where('active', true)->get()
+        ->each(function ($article) use ($sitemap, $defaultLocale) {
+            $blogPage = Page::where('type', 'blog')
+                ->where('lang_locale', $article->lang_locale)
+                ->first();
+            $blogSlug = $blogPage ? $blogPage->slug : 'blog';
+            
+            $articleUrl = $article->lang_locale === $defaultLocale
+                ? url('/' . $blogSlug . '/' . $article->slug)
+                : url('/' . $article->lang_locale . '/' . $blogSlug . '/' . $article->slug);
+            
+            $sitemap->add(Url::create($articleUrl));
+        });
+    
+    $sitemap->writeToFile(public_path('sitemap.xml'));
+    return "Sitemap vygenerován!";
+});
 
 Route::get('/{slug}', [PageController::class, 'show'])
     ->where('slug', '^(?!' . implode('|', $locales) . ')[a-zA-Z0-9\-\/]+$')
