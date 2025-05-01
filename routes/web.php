c4a20cf8c5d14ff7b8184eca77e363b9c239a8ea
filipkoudelnicek@seznam.cz <62012471+filipkoudelnicek@users.noreply.github.com>
2025-05-01 20:04:46 +0200

    Update sitemap.xml with new blog URLs and add route for sitemap generation

--- routes/web.php
+++ routes/web.php
@@ -18,29 +18,29 @@ Route::get('/', [PageController::class, 'homepage'])
 Route::get('/' . $blogSlug . '/{articleSlug}', [ArticleController::class, 'showArticle'])
     ->name('article.show');
 
+Route::get('/sitemap', function() {
+    return UrlService::generateSitemap();
+});
+
 Route::get('/{slug}', [PageController::class, 'show'])
     ->where('slug', UrlService::getLocalesRegex())
     ->name('page.show');
-    
+
 // Ostatní jazyky
 foreach ($languages as $language) {
     if ($language->locale !== $defaultLocale) {
         Route::get('/' . $language->locale, [PageController::class, 'homepage'])
             ->defaults('locale', $language->locale)
             ->name($language->locale . '.homepage');
-        
+
         $langBlogSlug = PageService::getBlogSlug($language->locale);
         Route::get('/' . $language->locale . '/' . $langBlogSlug . '/{articleSlug}', [ArticleController::class, 'showArticle'])
             ->defaults('locale', $language->locale)
             ->name($language->locale . '.article.show');
-        
+
         Route::get('/' . $language->locale . '/{slug}', [PageController::class, 'show'])
             ->defaults('locale', $language->locale)
             ->where('slug', '[a-zA-Z0-9\-\/]+')
             ->name($language->locale . '.page.show');
     }
 }
-
-Route::get('/sitemap', function() {
-    return UrlService::generateSitemap();
-});
