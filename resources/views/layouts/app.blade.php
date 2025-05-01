4ef7728a3efbdf5e94942e93827ca0af0b946b8a
filipkoudelnicek@seznam.cz <62012471+filipkoudelnicek@users.noreply.github.com>
2025-05-01 20:44:37 +0200

    Refactor CSS imports and clean up style comments for consistency

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" type="image/x-icon" href="/assets/images/fav.png">
+
+    @vite('resources/css/app.css')
     <!-- Bootstrap min css -->
     <link rel="stylesheet" href="/assets/css/vendor/fontawesome.css">
     <link rel="stylesheet" href="/assets/css/plugins/swiper.css">
@@ -16,7 +18,6 @@
     <link rel="stylesheet" href="/assets/css/vendor/metismenu.css">
     <!-- custom css -->
     <link rel="stylesheet" href="/assets/css/style.css">
-    @vite('resources/css/app.css')
 
     @yield('seo')
 
