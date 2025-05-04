0c02b93069b0447c37989b464353587c47bd339b
filipkoudelnicek@seznam.cz <62012471+filipkoudelnicek@users.noreply.github.com>
2025-05-04 22:36:31 +0200

    Add mobile menu

--- resources/views/components/header/header-menu.blade.php
+++ resources/views/components/header/header-menu.blade.php
@@ -24,6 +24,14 @@
                             </nav>
                         </div>
 
+                                <div class="tmp-side-collups-area" id="side-collups">
+                                    <svg width="20" height="16" viewBox="0 0 20 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <rect y="14" width="20" height="2" fill="#1F1F25"></rect>
+                                        <rect y="7" width="20" height="2" fill="#1F1F25"></rect>
+                                        <rect width="20" height="2" fill="#1F1F25"></rect>
+                                    </svg>
+                                </div>
+
                         <livewire:language-switcher />
 
                     </div>
@@ -32,3 +40,29 @@
         </div>
     </div>
 </header>
+
+<div id="side-hide">
+    <div class="top-area">
+        <a href="{{ $getHomepageUrl() }}" class="logo-area">
+            <img src="/assets/images/logo/logo-02.svg" alt="logo">
+        </a>
+        <livewire:language-switcher />
+        <div class="close-icon-area">
+            <div id="close-slide__main">
+                <i class="fa-solid fa-x"></i>
+            </div>
+        </div>
+    </div>
+    <div class="mobile-menu-main">
+        <nav class="nav-main mainmenu-nav mt--30">
+            <ul class="mainmenu metismenu" id="mobile-menu-active">  
+                @foreach($menuPages as $page)
+                    <li>
+                        <a href="{{ $getPageUrl($page) }}" class="main">{{ $page->in_menu_title ?? $page->title }}</a>
+                    </li>
+                @endforeach
+            </ul>
+        </nav>
+    </div>
+    <!-- mobile menu area end -->
+</div>
